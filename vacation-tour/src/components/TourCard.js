f1d8a8827afdc2333e3bd42fa4dcee5b7eb9a586
Thanh Le <thanhlevan0203@gmail.com>
2022-07-15 23:25:12 -0700

    Created theme for variant in Typography

--- vacation-tour/src/components/TourCard.js
+++ vacation-tour/src/components/TourCard.js
@@ -1,35 +1,84 @@
 import React from 'react';
-import { Paper, Grid, Typography, Box } from '@mui/material';
+import { Paper, Grid, Typography, Box, Rating, createTheme, ThemeProvider } from '@mui/material';
 import yosemity from '../images/yosemity.png';
 import { AccessTime } from '@mui/icons-material';
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: 'body2'
+          },
+          style: {
+            fontSize: 11,
+          }
+        },
+        {
+          props: {
+            variant: 'body3',
+          },
+          style: {
+            fontSize: 9,
+          }
+        }
+      ]
+    }
+  }
+})
 
 const TourCard = () => {
   return (
     <Grid item xs={3} >
-      <Paper elevation={3}>
-        <img src={yosemity}
-          alt=''
-          className='img'
-        />
-        <Box paddingX={1}>
-          <Typography variant='subtitle1' component='h2'>
-            Camping with Nature
-          </Typography>
-        </Box>
+        <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+            <img src={yosemity}
+            alt=''
+            className='img'
+            />
+            <Box paddingX={1}>
+            <Typography variant='subtitle1' component='h2'>
+                Camping with Nature
+            </Typography>
+            </Box>
 
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center'
-          }}
-        >
-          <AccessTime sx={{width: 12.5}} />
-          <Typography variant='body2' component='p' marginLeft={0.5}>
-            5 hours
-          </Typography>
-        </Box>
-        
-      </Paper>
+            <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center'
+            }}
+            >
+            <AccessTime sx={{width: 12.5}} />
+            <Typography variant='body2' component='p' marginLeft={0.5}>
+                5 hours
+            </Typography>
+            </Box>
+
+            <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+            }}
+            marginTop={3}
+            >
+            <Rating name='read-only' value={4.5} readOnly precision={0.5} size='small' />
+            <Typography variant='body2' component='p' marginLeft={0.5}>
+                4.5
+            </Typography>
+            <Typography variant='body3' component='p' marginLeft={0.5} size='small'>
+                (655 reviews)
+            </Typography>
+            </Box>
+
+            <Box>
+            <Typography variant='h6' component='h3' marginTop={1.0} >
+                From $100
+            </Typography>
+            </Box>
+            
+        </Paper>
+      </ThemeProvider>
     </Grid>
   )
 }
