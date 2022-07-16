19d01952adafa33cabac0b63b28740b889132250
Thanh Le <thanhlevan0203@gmail.com>
2022-07-16 14:35:02 -0700

    Added Image Lists in Tour page

--- vacation-tour/src/pages/Tour.js
+++ vacation-tour/src/pages/Tour.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Container } from '@mui/system'
-import { Typography } from '@mui/material'
+import { Typography, Container, Box, ImageList } from '@mui/material'
+import ImageLists from '../components/ImageLists'
+
 
 const Tour = () => {
   return (
@@ -8,6 +9,15 @@ const Tour = () => {
       <Typography variant='h3' component='h1' marginTop={3}>
         Explore the World in Vegas
       </Typography>
+
+      <Box marginTop={3} sx={{display: "flex"}}>
+        <img  
+          src='https://images.unsplash.com/photo-1577334928618-2ff2bf09e827?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3348&q=80'
+          alt=''
+          height={325}
+        />
+        <ImageLists />
+      </Box>
     </Container>
   )
 }
